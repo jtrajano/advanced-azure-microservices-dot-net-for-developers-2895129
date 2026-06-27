1593a3e66f775f85c98c20fd85645c42e3211755
jtrajano <jlt212000@yahoo.com>
2026-06-27 20:51:11 +0800

    register the domain event handler for PetFloggedForAdoptionIntegrationEvent

--- src/WisdomPetMedicine.Common/IIntegrationEvent.cs
+++ src/WisdomPetMedicine.Common/IIntegrationEvent.cs
@@ -1,6 +1,8 @@
 ﻿namespace WisdomPetMedicine.Common
 {
+
     public interface IIntegrationEvent
     {
+
     }
 }
--- src/WisdomPetMedicine.Pet.Api/ApplicationServices/PetApplicationService.cs
+++ src/WisdomPetMedicine.Pet.Api/ApplicationServices/PetApplicationService.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Extensions.Configuration;
 using System.Threading.Tasks;
 using WisdomPetMedicine.Pet.Api.Commands;
+using WisdomPetMedicine.Pet.Api.IntegrationEvents;
+using WisdomPetMedicine.Pet.Domain.Events;
 using WisdomPetMedicine.Pet.Domain.Repositories;
 using WisdomPetMedicine.Pet.Domain.Services;
 using WisdomPetMedicine.Pet.Domain.ValueObjects;
@@ -17,6 +19,21 @@ namespace WisdomPetMedicine.Pet.Api.ApplicationServices
         {
             this.petRepository = petRepository;
             this.breedService = breedService;
+
+            // Register the domain event handler for PetFlaggedForAdoption
+            DomainEvents.PetFlaggedForAdoption.Register(c =>
+            {
+                var integrationEvent = new PetFlaggedForAdoptionIntegrationEvent()
+                {
+                    Id = c.Id,
+                    Name = c.Name,
+                    Breed = c.Breed,
+                    Sex = c.Sex,
+                    Color = c.Color,
+                    DateOfBirth = c.DateOfBirth,
+                    Species = c.Species
+                };
+            });
         }
 
         public async Task HandleCommandAsync(CreatePetCommand command)
