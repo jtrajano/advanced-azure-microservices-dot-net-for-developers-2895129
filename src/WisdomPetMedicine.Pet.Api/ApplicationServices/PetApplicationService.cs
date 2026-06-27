bc0e249a56fd3ce16a9c24ddb749290e94d40061
jtrajano <jlt212000@yahoo.com>
2026-06-27 21:58:13 +0800

    test sending message  to service bus queue

--- src/WisdomPetMedicine.Pet.Api/ApplicationServices/PetApplicationService.cs
+++ src/WisdomPetMedicine.Pet.Api/ApplicationServices/PetApplicationService.cs
@@ -1,11 +1,19 @@
 ﻿using Microsoft.Extensions.Configuration;
 using System.Threading.Tasks;
+using WisdomPetMedicine.Common;
 using WisdomPetMedicine.Pet.Api.Commands;
 using WisdomPetMedicine.Pet.Api.IntegrationEvents;
 using WisdomPetMedicine.Pet.Domain.Events;
 using WisdomPetMedicine.Pet.Domain.Repositories;
 using WisdomPetMedicine.Pet.Domain.Services;
 using WisdomPetMedicine.Pet.Domain.ValueObjects;
+using System.Text.Json;
+using System.Text;
+using Azure.Messaging.ServiceBus;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using System.Net.Mime;
+using System;
+using Newtonsoft.Json;
 
 namespace WisdomPetMedicine.Pet.Api.ApplicationServices
 {
@@ -15,13 +23,15 @@ namespace WisdomPetMedicine.Pet.Api.ApplicationServices
         private readonly IBreedService breedService;
 
         public PetApplicationService(IPetRepository petRepository,
-                                     IBreedService breedService)
+                                     IBreedService breedService,
+                                     IConfiguration configuration
+                                     )
         {
             this.petRepository = petRepository;
             this.breedService = breedService;
 
             // Register the domain event handler for PetFlaggedForAdoption
-            DomainEvents.PetFlaggedForAdoption.Register(c =>
+            DomainEvents.PetFlaggedForAdoption.Register(async c =>
             {
                 var integrationEvent = new PetFlaggedForAdoptionIntegrationEvent()
                 {
@@ -33,6 +43,10 @@ namespace WisdomPetMedicine.Pet.Api.ApplicationServices
                     DateOfBirth = c.DateOfBirth,
                     Species = c.Species
                 };
+
+                await PublishIntegrationEventAsync(integrationEvent
+                    , configuration["ServiceBus:ConnectionString"]
+                    , configuration["ServiceBus:Adoption:QueueName"]);
             });
         }
 
@@ -79,6 +93,28 @@ namespace WisdomPetMedicine.Pet.Api.ApplicationServices
         {
             var pet = await petRepository.GetAsync(PetId.Create(command.Id));
             pet.TransferToHospital();
+        }
+
+        private async Task PublishIntegrationEventAsync(IIntegrationEvent integrationEvent, string connectionString, string queueName)
+        {
+            // Here you would publish the integration event to your message bus or event stream
+            // For example, using a message broker like RabbitMQ, Kafka, etc.
+            // This is a placeholder for the actual implementation
+            var jsonMessage = JsonConvert.SerializeObject(integrationEvent);
+            var body =  Encoding.UTF8.GetBytes(jsonMessage);
+            var client = new ServiceBusClient(connectionString);
+            await using ServiceBusSender sender = client.CreateSender(queueName);
+            var message = new ServiceBusMessage()
+            {
+                Body = new BinaryData(body),
+                ContentType = MediaTypeNames.Application.Json,
+                Subject = integrationEvent.GetType().FullName,
+                MessageId = Guid.NewGuid().ToString()
+            };
+
+            await sender.SendMessageAsync(message);
+
+
         }
     }
 }
