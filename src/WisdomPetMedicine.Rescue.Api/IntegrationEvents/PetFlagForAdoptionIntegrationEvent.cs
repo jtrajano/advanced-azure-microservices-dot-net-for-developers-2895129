20d29e0bc14be1c9e081fe6c8106d00ab3790375
jtrajano <jlt212000@yahoo.com>
2026-06-28 18:14:08 +0800

    chore: store data to RescuedAnimalsMetadata

--- src/WisdomPetMedicine.Rescue.Api/IntegrationEvents/PetFlagForAdoptionIntegrationEvent.cs
+++ src/WisdomPetMedicine.Rescue.Api/IntegrationEvents/PetFlagForAdoptionIntegrationEvent.cs
@@ -1,5 +1,14 @@
-﻿namespace WisdomPetMedicine.Rescue.Api.IntegrationEvents;
+﻿using System;
+
+namespace WisdomPetMedicine.Rescue.Api.IntegrationEvents;
 
 public class PetFlagForAdoptionIntegrationEvent
 {
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Breed { get; set; }
+    public int Sex { get; set; }
+    public string Color { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public string Species { get; set; }
 }
--- src/WisdomPetMedicine.Rescue.Api/IntegrationEvents/PetFlagForAdoptionIntegrationEventHandler.cs
+++ src/WisdomPetMedicine.Rescue.Api/IntegrationEvents/PetFlagForAdoptionIntegrationEventHandler.cs
@@ -1,24 +1,33 @@
 ﻿using Azure.Messaging.ServiceBus;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using System.Runtime.Serialization;
 using System.Threading;
 using System.Threading.Tasks;
+using WisdomPetMedicine.Rescue.Api.Infrastructure;
+using WisdomPetMedicine.Rescue.Domain.Entities;
+using WisdomPetMedicine.Rescue.Domain.Repositories;
+using WisdomPetMedicine.Rescue.Domain.ValueObjects;
 
 namespace WisdomPetMedicine.Rescue.Api.IntegrationEvents;
 
 public class PetFlagForAdoptionIntegrationEventHandler : BackgroundService
 {
     private readonly IConfiguration configuration;
+    private readonly IServiceScopeFactory serviceScopeFactory;
     private readonly ILogger<PetFlagForAdoptionIntegrationEventHandler> logger;
     private readonly ServiceBusClient serviceBusClient;
     private readonly ServiceBusProcessor serviceBusProcessor;
 
     public PetFlagForAdoptionIntegrationEventHandler(IConfiguration configuration, 
+        IServiceScopeFactory serviceScopeFactory,
         ILogger<PetFlagForAdoptionIntegrationEventHandler> logger)
     {
         this.configuration = configuration;
+        this.serviceScopeFactory = serviceScopeFactory;
         this.logger = logger;
 
         serviceBusClient = new ServiceBusClient(configuration["sb-pet-flag-for-adoption"]);
@@ -37,10 +46,16 @@ public class PetFlagForAdoptionIntegrationEventHandler : BackgroundService
     {
         var messageBody = arg.Message.Body.ToString();
         var theEvent = JsonConvert.DeserializeObject<PetFlagForAdoptionIntegrationEvent>(messageBody);
-        logger.LogInformation("Received message: {MessageBody}", messageBody);
-        // Here you can deserialize the message and handle it accordingly
-        // For example, you might want to call a service to process the adoption request
         await arg.CompleteMessageAsync(arg.Message);
+
+
+        using var scope = this.serviceScopeFactory.CreateScope();
+        var repo =scope.ServiceProvider.GetRequiredService<IRescueRepository>();
+        using var dbContext = scope.ServiceProvider.GetRequiredService<RescueDbContext>();
+        var rescuedAnimalMetadata = dbContext.RescuedAnimalsMetadata.Add(theEvent);
+        var rescuedAnimal = new RescuedAnimal(RescuedAnimalId.Create(theEvent.Id));
+        await repo.AddRescuedAnimalAsync(rescuedAnimal);
+
     }
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
