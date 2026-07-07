bc2e89be797b930fc82657f7f700246749555405
jtrajano <jlt212000@yahoo.com>
2026-07-07 21:43:09 +0800

    feat: add Phone number to Adopter entity feat: add setphonenumber handler

---
+++ src/WisdomPetMedicine.Rescue.Api/Commands/SetAdopterPhoneNumberCommand.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace WisdomPetMedicine.Rescue.Api.Commands;
+
+public class SetAdopterPhoneNumberCommand 
+{
+    public Guid Id { get; set; }
+
+    public string PhoneNumber { get; set; }
+}
---
+++ src/WisdomPetMedicine.Rescue.Domain/ValueObjects/AdopterPhoneNumber.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WisdomPetMedicine.Rescue.Domain.ValueObjects;
+
+public record AdopterPhoneNumber
+{
+    internal AdopterPhoneNumber(string value)
+    {
+        Value = value;
+    }
+
+    public string Value { get; init; }
+
+    public static AdopterPhoneNumber Create(string value)
+    {
+        Validate(value);
+        return new AdopterPhoneNumber(value);
+    }
+
+    public static void Validate(string value)
+    {
+        if (value == null)
+        {
+            throw new ArgumentNullException("Phone value must not be null");
+
+        }
+
+        if (value.Length > 15)
+        {
+            throw new ArgumentOutOfRangeException(nameof(value), "Phone number must be longer than 15 characters");
+        }
+    }
+}
